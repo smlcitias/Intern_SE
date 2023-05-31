e939e781a068ce31519e1999640086cbedd9e012
smlcitias <42274021+smlcitias@users.noreply.github.com>
2023-05-31 20:47:00 +0800

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,7 +1,10 @@
-export CUDA_VISIBLE_DEVICES='3'
+export CUDA_VISIBLE_DEVICES='0'
 
 python main.py --model BLSTM_01 --target MAP --batch_size 1 --epochs 50 \
-               --loss mse --version jin_0608 --lr 5e-4 --task VCTK --mode train --save_results False
+               --loss mse --version None --lr 5e-4 --task VCTK --mode train --save_results False
+               
+python main.py --model transfomerenconder --target MAP --batch_size 1 --epochs 100 \
+               --loss l1 --version None --lr 1e-5 --task VCTK --mode train --save_results False
                
 # python main.py --model BLSTM_02 --target IRM --batch_size 1 --epochs 50 \
 #                --loss mse --version jin_0608 --lr 5e-4 --task VCTK
