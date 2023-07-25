601576fda0f8f6e6308669f6c8c137c90f17233b
smlcitias <42274021+smlcitias@users.noreply.github.com>
2023-07-25 16:07:14 +0800

    Update run1.sh
    Thanks to 以謙 for helping to find the wrong word bug.

--- run1.sh
+++ run1.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES='0'
 python main.py --model BLSTM_01 --target MAP --batch_size 1 --epochs 50 \
                --loss mse --version None --lr 5e-4 --task VCTK --mode train --save_results False
                
-python main.py --model transfomerenconder --target MAP --batch_size 1 --epochs 100 \
+python main.py --model transformerencoder --target MAP --batch_size 1 --epochs 100 \
                --loss l1 --version None --lr 1e-5 --task VCTK --mode train --save_results False
                
 # python main.py --model BLSTM_02 --target IRM --batch_size 1 --epochs 50 \
